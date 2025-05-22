a149b764ca53a5c60beef15219ea8e7b58e68745
David Revivaldy <davidrsitorus04@gmail.com>
2025-05-22 22:55:00 +0700

    - Fix Splascreen - Remove Action Bar

--- app/src/main/java/com/example/talentara/view/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/talentara/view/ui/main/MainActivity.kt
@@ -1,14 +1,10 @@
 package com.example.talentara.view.ui.main
 
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.talentara.R
 import com.example.talentara.databinding.ActivityMainBinding
@@ -25,19 +21,9 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
-
         val navView: BottomNavigationView = binding.navView
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_history, R.id.navigation_notification, R.id.navigation_profile
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
 }

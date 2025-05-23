8360945c2162b17bf60f8d04e47aabffef1e2d49
David Revivaldy <davidrsitorus04@gmail.com>
2025-05-23 21:32:06 +0700

    Slicing Notification Page

--- app/src/main/java/com/example/talentara/view/ui/notifications/NotificationFragment.kt
+++ app/src/main/java/com/example/talentara/view/ui/notifications/NotificationFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.talentara.databinding.FragmentNotificationBinding
@@ -20,18 +19,13 @@ class NotificationFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
-        savedInstanceState: Bundle?
+        savedInstanceState: Bundle?,
     ): View {
         val notificationsViewModel =
             ViewModelProvider(this)[NotificationsViewModel::class.java]
 
         _binding = FragmentNotificationBinding.inflate(inflater, container, false)
         val root: View = binding.root
-
-        val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
         return root
     }
 

665e84d7b2b479fbb4d22963d6e9eee566f93370
David Revivaldy <davidrsitorus04@gmail.com>
2025-05-27 00:57:27 +0700

    Slicing Profile Page

--- app/src/main/java/com/example/talentara/view/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/talentara/view/ui/profile/ProfileFragment.kt
@@ -4,39 +4,52 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.talentara.R
 import com.example.talentara.databinding.FragmentProfileBinding
+import com.example.talentara.view.ui.profile.talent.TalentProfileFragment
+import com.example.talentara.view.ui.profile.user.UserProfileFragment
+import com.google.android.material.tabs.TabLayoutMediator
 
 class ProfileFragment : Fragment() {
 
     private var _binding: FragmentProfileBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
     ): View {
-        val profileViewModel =
-            ViewModelProvider(this)[ProfileViewModel::class.java]
-
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val textView: TextView = binding.textDashboard
-        profileViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
+        return binding.root
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupViewPagerWithTabs()
+    }
+
+    private fun setupViewPagerWithTabs() {
+        val adapter = object : FragmentStateAdapter(this) {
+            private val pages = listOf(
+                UserProfileFragment(),
+                TalentProfileFragment()
+            )
+
+            override fun getItemCount() = pages.size
+            override fun createFragment(position: Int) = pages[position]
+        }
+        binding.viewPager.adapter = adapter
+
+        TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, pos ->
+            tab.text = if (pos == 0)
+                getString(R.string.user_profile)
+            else
+                getString(R.string.talent_profile)
+        }.attach()
+
+        binding.viewPager.currentItem = 0
     }
 }

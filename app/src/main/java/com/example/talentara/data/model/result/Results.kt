292b16af29fd46fd0abb6630a745373e906037a8
David Revivaldy <davidrsitorus04@gmail.com>
2025-06-17 17:33:10 +0700

    Setup Results

--- app/src/main/java/com/example/talentara/data/model/result/Results.kt
+++ app/src/main/java/com/example/talentara/data/model/result/Results.kt
@@ -1,4 +1,7 @@
 package com.example.talentara.data.model.result
 
-class Results {
+sealed class Results<out R> private constructor(){
+    data class Success<out T>(val data: T): Results<T>()
+    data class Error(val error: String): Results<Nothing>()
+    data object Loading: Results<Nothing>()
 }
